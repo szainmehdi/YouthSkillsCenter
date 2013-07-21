912fdb855a8534c08478592e710bc1e0aa47b7f2
S. Zain Mehdi <szainmehdi@gmail.com>
2013-07-20 22:18:44 -0700

    Setup basic redesign.

--- index.php
+++ index.php
@@ -138,41 +138,50 @@
                     <div class="content-box">
 
                     </div>
+                    <div class="content-box small-left">
+
+                    </div>
+                    <div class="content-box large-right">
+
+                    </div>
+
+                    <!-- FOOTER -->
+                    <div id="footer-wrap">
+                        <div id="footer">
+                            <!-- copyright & related links go here -->
+                            <div class="footer-links" id="footer-links1">
+                                <h4>Information</h4>
+                                <a class="fl" href="#">About YSC</a>
+                                <a class="fl" href="#">Our Staff</a>
+                                <a class="fl" href="#">Our Facility</a>
+                                <h4>MyYSC</h4>
+                                <a class="fl" href="#">Login to MyYSC</a>
+                                <a class="fl" href="#">Learn more about MyYSC</a>
+                                <a class="fl" href="#">Sign up for MyYSC</a>
+                            </div>
+                            <div class="footer-links" id="footer-links2">
+                                <h4>Site Info</h4>
+                                <a class="fl" href="#">Privacy Policy</a>
+                                <a class="fl" href="#">Terms of Service</a>
+                                <a class="fl" href="#">Feedback</a>
+                                <h4>Resources</h4>
+                                <a class="fl" href="#">ABCya.com Games</a>
+                                <a class="fl" href="#">Multiplication.com Games</a>
+                                <a class="fl" href="#"><em>more</em></a>
+                            </div>
+                            <div class="footer-links" id="footer-links2">
+                                <img src="images/logos/site-logo-thumb.png" alt="YSC" />
+                                <h4>Contact Us</h4>
+                                <a class="fl" href="tel:9516812972"><strong>tel.</strong> 951.681.2972</a>
+                                <a class="fl" href="#"><strong>email.</strong> info@ysc5.com</a>
+                                <a class="fl" href="#"><strong>location.</strong> 5111 Felspar Street,<br /><span id="cty">Jurupa Valley, CA 92509</span></a>
+                            </div>
+                            <p>Copyright &copy; <?php echo date("Y"); ?> Youth Skills Center. All images, artwork, and logos used in compliance or with permission. Site by <a href="http://www.zcomputers.org" target="_blank">Z Computers</a>. Designed and Engineered by S. Zain Mehdi.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        	<div id="footer-wrap">
-            	<div id="footer">
-                	<!-- copyright & related links go here -->
-                    <div class="footer-links" id="footer-links1">
-                    	<h4>Information</h4>
-                        <a class="fl" href="#">About YSC</a>
-                        <a class="fl" href="#">Our Staff</a>
-                        <a class="fl" href="#">Our Facility</a>
-                        <h4>MyYSC</h4>
-                        <a class="fl" href="#">Login to MyYSC</a>
-                        <a class="fl" href="#">Learn more about MyYSC</a>
-                        <a class="fl" href="#">Sign up for MyYSC</a>
-                    </div>
-                    <div class="footer-links" id="footer-links2">
-                        <h4>Site Info</h4>
-                        <a class="fl" href="#">Privacy Policy</a>
-                        <a class="fl" href="#">Terms of Service</a>
-                        <a class="fl" href="#">Feedback</a>
-                        <h4>Resources</h4>
-                        <a class="fl" href="#">ABCya.com Games</a>
-                        <a class="fl" href="#">Multiplication.com Games</a>
-                        <a class="fl" href="#"><em>more</em></a>
-                    </div>
-                    <div class="footer-links" id="footer-links2">
-                    	<img src="images/logos/site-logo-thumb.png" alt="YSC" />
-                        <h4>Contact Us</h4>
-                        <a class="fl" href="tel:9516812972"><strong>tel.</strong> 951.681.2972</a>
-                        <a class="fl" href="#"><strong>email.</strong> info@ysc5.com</a>
-                        <a class="fl" href="#"><strong>location.</strong> 5111 Felspar Street,<br /><span id="cty">Jurupa Valley, CA 92509</span></a>
-                    </div>
-                    <p>Copyright &copy; <?php echo date("Y"); ?> Youth Skills Center. All images, artwork, and logos used in compliance or with permission. Site by <a href="http://www.zcomputers.org" target="_blank">Z Computers</a>. Designed and Engineered by S. Zain Mehdi.</p>
-                </div>
-            </div>
+
             </div>
     </body>
 </html>
