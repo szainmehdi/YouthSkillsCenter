0b600a4122cd1508e7b916c79c7a86ffe2a8fb36
Zain Mehdi <szainmehdi@gmail.com>
2013-08-01 18:39:29 -0700

    Separated footer

--- index.php
+++ index.php
@@ -33,14 +33,15 @@ $cfg['page'] = "home";
             <div id="content-wrap">
             	<div id="content-inner-wrap">
                     <div class="content-box">
-                        <div id="large_banner_test_01" class="banner_image" style="height: 400px;background-image: url(images/banners/banner-1.jpg)">
+                        <div id="large_banner_test_01" class="banner_image" style="height: 400px;background-image: url(images/banners/child-hands_large.jpg);
+                        background-position: 150px -200px;">
                             <div class="banner_content">
-                                <h1 style="background-color: rgba(98, 169, 255, 0.86);color: white;margin-top: 120px;">
-                                    Best. Summer. Ever.
+                                <h1 style="background-color: rgba(255,8,123,0.84);color: white;margin-top: 120px;">
+                                    Unleash your child's potential.
                                 </h1>
                                 <p>
-                                    Enroll now in YSC's 10 week Summer Camp program for the
-                                    most enjoyable learning experience.
+                                    With the warm and welcoming environment at YSC, your child reach
+                                    their true potential. And with the best prices of the year, now is the best time to join!
                                 </p>
                                 <div class="action_button">
                                     <div class="icon"></div>
@@ -72,6 +73,24 @@ $cfg['page'] = "home";
                             </p>
                         </div>
                     </div>
+                    <div class="content-box">
+                        <div id="large_banner_test_01" class="banner_image" style="height: 400px;background-image: url(images/banners/banner-1.jpg)">
+                            <div class="banner_content">
+                                <h1 style="background-color: rgba(98, 169, 255, 0.86);color: white;margin-top: 120px;">
+                                    Best. Summer. Ever.
+                                </h1>
+                                <p>
+                                    Enroll now in YSC's 10 week Summer Camp program for the
+                                    most enjoyable learning experience.
+                                </p>
+                                <div class="action_button">
+                                    <div class="icon"></div>
+                                    Register Now!
+                                    <a href="#"><span class="full-box-link"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div class="content-box" style="background-color: #1FCB4A;">
                         <div class="content-box-inner-wrap">
                             <h2 style="color: white;"><strong>Enroll Now</strong> for Preschool and Child Care!</h2>
@@ -82,61 +101,8 @@ $cfg['page'] = "home";
                             </p>
                         </div>
                     </div>
-                    <div class="content-box">
-                        <div id="large_banner_test_01" class="banner_image" style="height: 400px;background-image: url(images/banners/child-hands_large.jpg);
-                        background-position: 150px -200px;">
-                            <div class="banner_content">
-                                <h1 style="background-color: rgba(255,8,123,0.84);color: white;margin-top: 120px;">
-                                    Unleash your child's potential.
-                                </h1>
-                                <p>
-                                    With the warm and welcoming environment at YSC, your child reach
-                                    their true potential. And with the best prices of the year, now is the best time to join!
-                                </p>
-                                <div class="action_button">
-                                    <div class="icon"></div>
-                                    Register Now!
-                                    <a href="#"><span class="full-box-link"></span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- FOOTER -->
-                    <div id="footer-wrap">
-                        <div id="footer">
-                            <!-- copyright & related links go here -->
-                            <div class="footer-links" id="footer-links1">
-                                <h4>Information</h4>
-                                <a class="fl" href="#">About YSC</a>
-                                <a class="fl" href="#">Our Staff</a>
-                                <a class="fl" href="#">Our Facility</a>
-                                <h4>MyYSC</h4>
-                                <a class="fl" href="#">Login to MyYSC</a>
-                                <a class="fl" href="#">Learn more about MyYSC</a>
-                                <a class="fl" href="#">Sign up for MyYSC</a>
-                            </div>
-                            <div class="footer-links" id="footer-links2">
-                                <h4>Site Info</h4>
-                                <a class="fl" href="#">Privacy Policy</a>
-                                <a class="fl" href="#">Terms of Service</a>
-                                <a class="fl" href="#">Feedback</a>
-                                <h4>Resources</h4>
-                                <a class="fl" href="#">ABCya.com Games</a>
-                                <a class="fl" href="#">Multiplication.com Games</a>
-                                <a class="fl" href="#"><em>more</em></a>
-                            </div>
-                            <div class="footer-links" id="footer-links2">
-                                <img src="images/logos/site-logo-thumb.png" alt="YSC" />
-                                <h4>Contact Us</h4>
-                                <a class="fl" href="tel:9516812972"><strong>tel.</strong> 951.681.2972</a>
-                                <a class="fl" href="#"><strong>email.</strong> info@ysc5.com</a>
-                                <a class="fl" href="#"><strong>location.</strong> 5111 Felspar Street,<br /><span id="cty">Jurupa Valley, CA 92509</span></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="copyright">
-                        <p>Copyright &copy; <?php echo date("Y"); ?> Youth Skills Center. All images, artwork, and logos used in compliance or with permission. Site by <a href="http://www.zcomputers.org" target="_blank">Z Computers</a>. Designed and Engineered by S. Zain Mehdi.</p>
-                    </div>
+
+                    <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/YouthSkillsCenter/includes/footer.inc.php'); ?>
                 </div>
             </div>
 
