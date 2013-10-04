8616c39a374ccc6cacd7eb412771ac3adaf34a0a
Zain Mehdi <szainmehdi@gmail.com>
2013-10-04 00:23:25 -0700

    fixed small issue

--- functions/php/admin.createDefaultUser.php
+++ functions/php/admin.createDefaultUser.php
@@ -3,6 +3,7 @@ require_once $cfg['root_dir'] . 'includes/global.inc.php';
 
 $usr = new User();
 $usr->edit(array(
+    "name" => "S. Zain Mehdi",
     "email" => "admin@ysc5.com",
     "password" => "SyedM5111",
     "type" => 9
