45eb597da5926cd3443f3b36947570c469d425da
S. Zain Mehdi <szainmehdi@gmail.com>
2014-11-03 22:37:31 -0800

    Updated seal

--- app/views/layout/my-ysc.blade.php
+++ app/views/layout/my-ysc.blade.php
@@ -18,7 +18,7 @@
                 <div class="col-md-3 my-ysc-nav">
                     @yield('my-ysc-nav')
 
-                    <a href="https://www.positivessl.com" style="margin-top: 24px;font-family: arial; font-size: 10px; color: #212121; text-decoration: none;"><img src="https://www.positivessl.com/images-new/PositiveSSL_tl_white2.png" alt="SSL Certificate" title="SSL Certificate" border="0" /></a>
+                    <a href="https://www.positivessl.com" style="margin-top: 24px;font-family: arial; font-size: 10px; color: #212121; text-decoration: none;display: block;text-align: center"><img src="https://www.positivessl.com/images-new/PositiveSSL_tl_white2.png" alt="SSL Certificate" title="SSL Certificate" border="0" /></a>
                 </div>
                 <div class="col-md-8 col-md-offset-1">
                     @yield('my-ysc-content')
